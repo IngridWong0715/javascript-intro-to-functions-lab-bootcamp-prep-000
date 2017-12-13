6f065e0ffd7bd812cf086585653f36167017d758
Ingrid Wong <yw340@georgetown.edu>
2017-12-13 21:21:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ function shout(string){
 function whisper(string){
   return string.toLowerCase();
 }
-/*describe('whisper(string)', function() {
-  it('receives one argument and returns it in all lowercase', function() {
-    expect(whisper('HELLO')).toEqual('hello')
-  })
-})
+
+function logShout(string){
+  
+}
+/*
 
 describe('logShout(string)', function() {
   it('calls console.log() its one argument in all caps', function() {
