d6079b838cd878cab4a64e5beb0c0abf3dfce6eb
Ingrid Wong <yw340@georgetown.edu>
2017-12-13 21:26:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,10 +15,13 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if (string === shout(string)){
+  if (string === whisper(string)){
     return "I can\'t hear you!";
+  } else if (string === shout(string)){
+    return "YES INDEED";
+  } else if (string === "I love you, Grandma"){
+    return "I love you, too";
   }
-  return "YES INDEED";
 }
 /*
 
